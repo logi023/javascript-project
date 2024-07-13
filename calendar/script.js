02b9cf90d7cb57e6e2cf363204a85dbb611375f2
eeseul <sseul2323@gmail.com>
2024-07-13 21:11:54 +0900

    달력 이전달/다음달 구현, 동작들 함수로 관리하기

--- calendar/script.js
+++ calendar/script.js
@@ -1,34 +1,38 @@
+const newDdate = new Date();
+let beforeMonth;
+let beforeYear;
+let NextMonth;
+let NextYear;
+let thisMonth;
+let thisYear;
 
-
-window.onload = function() {
-    const newDdate = new Date();
-    const thisYear = newDdate.getFullYear();
-    const thisMonth = newDdate.getMonth() + 1;
-
-    document.querySelector('.year').innerText = thisYear;
+const loadCalendarTitle = (thisMonth, thisYear) => {
     document.querySelector('.month').innerText = thisMonth;
-    // console.log(thisYear)
-    // console.log(thisMonth)
+    document.querySelector('.year').innerText = thisYear;
 
-    // console.log(new Date(date.getFullYear(), date.getMonth() + 1, 0)); // 현재 달의 마지막 날을 구함. --> Wed Jul 31 2024 00:00:00 GMT+0900 (한국 표준시)
-    // console.log((new Date(date.getFullYear(), date.getMonth() + 1, 0)).getDate()) // 현재 달의 마지막 날의 '날짜'를 구함. --> 31
-    // console.log((new Date(date.getFullYear(), date.getMonth() + 1, 0)).getDay()) // 0은 일요일, 1은 월요일 ... 6은 토요일 --> 3
+    loadCalendarDates(thisMonth, thisYear);
+    checkToday(thisMonth, thisYear);
+}
 
+const loadCalendarDates = (thisMonth, thisYear) => {
     const thisMonthLast = new Date(thisYear, thisMonth, 0); // 현재 월의 마지막 날을 구함.
     const thisMonthLastDate = thisMonthLast.getDate(); // 이번 달의 마지막 날짜를 가져옴
 
     const prevMonthLast = new Date(thisYear, thisMonth - 1, 0); // 지난 달의 마지막 날
     const prevMonthLastDate = prevMonthLast.getDate(); // 지난 달의 마지막 날짜를 가져옴
     const prevMonthLastDay = prevMonthLast.getDay() + 1; // 지난 달의 마지막 요일 가져옴
-    // console.log(prevMonthLast)
-    // console.log(prevMonthLastDay)
 
     const datesArray = Array.from({ length: thisMonthLastDate }, (_, index) => index + 1); // 첫번째 인자 _는 현재 요소의 값. 여기서는 중요하지 않기 때문에 이 매개변수를 사용하지 않는 의미로 자주 사용되는 관용표현임.
-    for (let i = 0; i<prevMonthLastDay; i++) {
-        datesArray.unshift('');
+    if (prevMonthLastDay <= 6){
+        for (let i = 0; i<prevMonthLastDay; i++) {
+            datesArray.unshift('');
+        }
     }
-    // console.log(datesArray)
+    
     const dates = document.querySelector('.dates');
+    while (dates.firstChild) {
+        dates.removeChild(dates.firstChild);
+    }
     datesArray.forEach(i => {
         let div = document.createElement('div');
         let span = document.createElement('span');
@@ -37,11 +41,10 @@ window.onload = function() {
         dates.appendChild(div);
         div.appendChild(span);
     })
+}
 
-    // 오늘 날짜 구하기
+const checkToday = (thisMonth, thisYear) => {
     const today = new Date();
-    console.log(thisMonth, today.getMonth() + 1)
-    console.log(thisYear, today.getFullYear())
     if (thisMonth === (today.getMonth() + 1) && thisYear === today.getFullYear()) {
         for (let date of document.querySelectorAll('.date')){
             if(parseInt(date.innerText) === today.getDate()){
@@ -50,4 +53,41 @@ window.onload = function() {
             }
         }
     }
+}
+
+const loadBeforeMonth = () => {
+    let beforeMonth = thisMonth - 1;
+    if (beforeMonth < 1) {
+        beforeMonth = 12;
+        thisMonth = beforeMonth;
+        thisYear = thisYear - 1;
+        loadCalendarTitle(thisMonth, thisYear);
+    } else {
+        thisMonth = beforeMonth;
+        loadCalendarTitle(thisMonth, thisYear);
+    }
+};
+
+const loadNextMonth = () => {
+    let nextMonth = thisMonth + 1;
+    if (nextMonth > 12) {
+        nextMonth = 1;
+        thisMonth = nextMonth;
+        thisYear = thisYear + 1;
+        loadCalendarTitle(thisMonth, thisYear);
+    } else {
+        thisMonth = nextMonth;
+        loadCalendarTitle(thisMonth, thisYear);
+    }
+}
+
+window.onload = function() {
+    thisMonth = newDdate.getMonth() + 1;
+    thisYear = newDdate.getFullYear();
+    loadCalendarTitle(thisMonth, thisYear);
+
+    const beforeMonthBtn = document.querySelector('.nav-before');
+    beforeMonthBtn.addEventListener('click', loadBeforeMonth);
+    const nextMonthBtn = document.querySelector('.nav-after');
+    nextMonthBtn.addEventListener('click', loadNextMonth);
 }
